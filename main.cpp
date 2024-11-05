27d93563095fcb68e64c39695791015babc63ab4
zzzzzzzZz-chow <1416271215@qq.com>
2024-11-06 01:07:59 +0800

    支持负数

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <number.h>
+#include <chrono>
 
 
 int main() {
 
     std::cout  << "Ehlo, world!" << std::endl;
  
-    Number n1("0000");
+    Number n1("-00101");
     Number n2("005");
- //   for(int i = 0; i < 1000000; i++)
-        Number n3 = n1 - n2;
+    auto now = std::chrono::steady_clock::now();
+    Number n3("0");
+    for(int i = 0; i < 1000000; i++)
+        n3 = n1 * n2;
+        std::cout << (std::chrono::steady_clock::now() - now) / 1000000000. << "s\n";
     std::cout << n3.num << std::endl;
     Number n4("0");
     Number n5("50000000");
--- src/number.cpp
+++ src/number.cpp
@@ -8,10 +8,16 @@ Number::Number(const std::string& numstr) {
 	num = numstr;
 }
 
-bool IsNumberNegative(const Number& num);
+bool IsNumberNegative(const Number& num) {
+	if (num.num[0] == '-') {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 Number Number::operator+(const Number& other) const {
-
 	if (IsNumberNegative(num) && !IsNumberNegative(other)) {
 		std::string tmpnum = num;
 		tmpnum.erase(0, 1);
@@ -27,69 +33,59 @@ Number Number::operator+(const Number& other) const {
 		std::string tmpnum2 = other.num;
 		tmpnum1.erase(0, 1);
 		tmpnum2.erase(0, 1);
-		return Number('-'+(Number(tmpnum1) + Number(tmpnum2)).num);
+		return Number('-' + (Number(tmpnum1) + Number(tmpnum2)).num);
+	}
+	if (num.size() > 1 && num[0] == '0') {
+		std::string tmpnum1 = num;
+		tmpnum1.erase(0, 1);
+		return Number(tmpnum1) + other;
+	}
+	if (other.num.size() > 1 && other.num[0] == '0') {
+		std::string tmpnum2 = other.num;
+		tmpnum2.erase(0, 1);
+		return *this + Number(tmpnum2);
 	}
 
-		std::string tmpstr = "";
-		int Ncount = 0;
-		int index = 0;
-		for (index = 0; index < std::max(num.size(), other.num.size()); index++){
-//			std::cout << "index:" << index << std::endl;
-			char tmpnum = '0';
-			char tmpother = '0';
-			char tmpaddtion = '0';
-			if (int(num.size()) - 1 - index >= 0) {
-				tmpnum=num[num.size() - 1 - index];
-			}			
-			if (int(other.num.size()) - 1 - index >= 0) {
-				tmpother = other.num[other.num.size() - 1 - index];
-			}
-			if (char(tmpnum + tmpother - '0') > '9') {
-				tmpaddtion = tmpnum + tmpother - '0' - 10 + Ncount;
-				Ncount = 1;
+	std::string tmpstr = "";
+	int Ncount = 0;
+	int index = 0;
+	for (index = 0; index < std::max(num.size(), other.num.size()); index++) {
+		char tmpnum = '0';
+		char tmpother = '0';
+		char tmpaddtion = '0';
+		if (int(num.size()) - 1 - index >= 0) {
+			tmpnum = num[num.size() - 1 - index];
+		}
+		if (int(other.num.size()) - 1 - index >= 0) {
+			tmpother = other.num[other.num.size() - 1 - index];
+		}
+		if (char(tmpnum + tmpother - '0') > '9') {
+			tmpaddtion = tmpnum + tmpother - '0' - 10 + Ncount;
+			Ncount = 1;
+		}
+		else {
+			if (char(tmpnum + tmpother - '0' + Ncount) > '9') {
+				tmpaddtion = tmpnum + tmpother - '0' + Ncount - 10;
+				Ncount = 1;//连续进位
 			}
 			else {
-				if (char(tmpnum + tmpother - '0' + Ncount) > '9') {
-					tmpaddtion = tmpnum + tmpother - '0' + Ncount - 10;
-					Ncount = 1;//连续进位
-				}
-				else {
-					tmpaddtion = tmpnum + tmpother - '0' + Ncount;
-					Ncount = 0;
-				}
+				tmpaddtion = tmpnum + tmpother - '0' + Ncount;
+				Ncount = 0;
 			}
-			tmpstr = tmpaddtion + tmpstr;
 		}
-		if (Ncount != 0) {
-			tmpstr = char('0' + Ncount) + tmpstr;
-		}
-		return Number(tmpstr);
-}
-
-//std::string Numberversal(const std::string& inputstring){
-//	std::string tmpstr = "";
-//	int index = 0;
-//	for (index = 0; index < inputstring.size(); index++) {
-//		tmpstr= tmpstr+char('0'+':'-inputstring[index]);
-//	}
-//	return tmpstr;
-//}
-
-bool IsNumberNegative(const Number& num) {
-	if (num.num[0] == '-') {
-		return true;
+		tmpstr = tmpaddtion + tmpstr;
 	}
-	else {
-		return false;
+	if (Ncount != 0) {
+		tmpstr = char('0' + Ncount) + tmpstr;
 	}
+	return Number(tmpstr);
 }
 
 Number Number::operator-(const Number& other) const {
-	
 	if (IsNumberNegative(num) && !IsNumberNegative(other)) {
 		std::string tmpnum = num;
 		tmpnum.erase(0, 1);
-		return Number('-'+(Number(other + Number(tmpnum)).num));
+		return Number('-' + (Number(other + Number(tmpnum)).num));
 	}
 	if (!IsNumberNegative(num) && IsNumberNegative(other)) {
 		std::string tmpnum = other.num;
@@ -103,7 +99,7 @@ Number Number::operator-(const Number& other) const {
 		tmpnum2.erase(0, 1);
 		return Number(Number(tmpnum2) - Number(tmpnum1));
 	}
-	if (num.size() > 1 &&num[0] == '0') {
+	if (num.size() > 1 && num[0] == '0') {
 		std::string tmpnum1 = num;
 		tmpnum1.erase(0, 1);
 		return Number(tmpnum1) - other;
@@ -118,24 +114,23 @@ Number Number::operator-(const Number& other) const {
 	int index = 0;
 
 	if (num.size() < other.num.size()) {
-		return Number("-"+(other-Number(num)).num);
+		return Number("-" + (other - Number(num)).num);
 	}
 	if (num.size() == other.num.size()) {
 		for (index = 0; index < num.size(); index++) {
-			if(num[index] < other.num[index])
+			if (num[index] < other.num[index])
 				return Number("-" + (other - Number(num)).num);
 			if (num[index] > other.num[index])
 				break;
 		}
 	}
-	
-	for (index = 0; index < std::max(num.size(), other.num.size()); index++){
-//		std::cout << "index:" << index << std::endl;
+
+	for (index = 0; index < std::max(num.size(), other.num.size()); index++) {
 		char tmpnum = '0';
 		char tmpother = '0';
 		char tmpsubtraction = '0';
 		if (int(num.size()) - 1 - index >= 0) {
-			tmpnum=num[num.size() - 1 - index];
+			tmpnum = num[num.size() - 1 - index];
 		}
 		if (int(other.num.size()) - 1 - index >= 0) {
 			tmpother = other.num[other.num.size() - 1 - index];
@@ -150,10 +145,10 @@ Number Number::operator-(const Number& other) const {
 				Ncount = 0;
 			}
 		}
-			tmpstr = tmpsubtraction + tmpstr;
+		tmpstr = tmpsubtraction + tmpstr;
 	}
 	for (int i = 0; i < tmpstr.size(); i++) {
-		if (tmpstr[i] == '0'&& tmpstr.size()!=1) {
+		if (tmpstr[i] == '0' && tmpstr.size() != 1) {
 			tmpstr.erase(0, 1);
 			i--;
 		}
@@ -161,43 +156,18 @@ Number Number::operator-(const Number& other) const {
 			break;
 		}
 	}
-	//if (Ncount != 0) {
-	//	tmpstr = Numberversal(tmpstr);
-	//	tmpstr = char('-') + tmpstr;
-	//}
-
 	return Number(tmpstr);
 }
-
-
-//Number Number::operator*(const Number& other) const {
-//	std::string tmpstr = other.num;
-//	Number outputCountstr = { "0" };
-//	Number tmpCountstr01 = Number(num);
-//	for (int i = 0; i < tmpstr.size(); i++) {
-//
-//		for (int j = 0; j < tmpstr[i] - '0'; j++) {
-//			outputCountstr = Number(outputCountstr) + tmpCountstr01;
-//		}
-//
-//		Number tmpCountstr02 = outputCountstr;
-//
-//		outputCountstr = Number(outputCountstr.num + "0");
-//	}
-//	outputCountstr.num.erase(outputCountstr.num.end() - 1);
-//	return  outputCountstr;
-//}
-
 
 static Number partialMultiply(const Number& other, const char& digit) {
 	Number result = other;
 	int int_digit = digit - '0';
-	for (auto & it : result.num) {
+	for (auto& it : result.num) {
 		it = unsigned char((it - '0') * int_digit + '0');
 	}
 	int carry = 0;
-	for (auto it = result.num.rbegin(); it!= result.num.rend(); it++) {
-		if (unsigned char(*it + carry)  > '9') {
+	for (auto it = result.num.rbegin(); it != result.num.rend(); it++) {
+		if (unsigned char(*it + carry) > '9') {
 			int one_num = (unsigned char(*it + carry) - '0');
 			carry = one_num / 10;
 			*it = one_num % 10 + '0';
@@ -214,6 +184,33 @@ static Number partialMultiply(const Number& other, const char& digit) {
 }
 
 Number Number::operator*(const Number& other) const {
+	if (IsNumberNegative(num) && !IsNumberNegative(other)) {
+		std::string tmpnum = num;
+		tmpnum.erase(0, 1);
+		return '-' + (other * Number(tmpnum)).num;
+	}
+	if (!IsNumberNegative(num) && IsNumberNegative(other)) {
+		std::string tmpnum = other.num;
+		tmpnum.erase(0, 1);
+		return '-' + (Number(num) * tmpnum).num;
+	}
+	if (IsNumberNegative(num) && IsNumberNegative(other)) {
+		std::string tmpnum1 = num;
+		std::string tmpnum2 = other.num;
+		tmpnum1.erase(0, 1);
+		tmpnum2.erase(0, 1);
+		return Number(tmpnum1) * Number(tmpnum2);
+	}
+	if (num.size() > 1 && num[0] == '0') {
+		std::string tmpnum1 = num;
+		tmpnum1.erase(0, 1);
+		return Number(tmpnum1) * other;
+	}
+	if (other.num.size() > 1 && other.num[0] == '0') {
+		std::string tmpnum2 = other.num;
+		tmpnum2.erase(0, 1);
+		return *this * Number(tmpnum2);
+	}
 	Number result = { "0" };
 	for (auto it : other.num) {
 		result = result + partialMultiply(num, it);
@@ -261,15 +258,58 @@ bool Number::operator>=(const Number& other) const {
 
 //数的除法
 Number Number::operator/(const Number& other) const {
+	if (IsNumberNegative(num) && !IsNumberNegative(other)) {
+		std::string tmpnum = num;
+		tmpnum.erase(0, 1);
+		return '-' + (Number(tmpnum) / other).num;
+	}
+	if (!IsNumberNegative(num) && IsNumberNegative(other)) {
+		std::string tmpnum = other.num;
+		tmpnum.erase(0, 1);
+		return '-' + (Number(num) / tmpnum).num;
+	}
+	if (IsNumberNegative(num) && IsNumberNegative(other)) {
+		std::string tmpnum1 = num;
+		std::string tmpnum2 = other.num;
+		tmpnum1.erase(0, 1);
+		tmpnum2.erase(0, 1);
+		return Number(tmpnum1) / Number(tmpnum2);
+	}
+	if (IsNumberNegative(num) && !IsNumberNegative(other)) {
+		std::string tmpnum = num;
+		tmpnum.erase(0, 1);
+		return Number(other - Number(tmpnum)).num;
+	}
+	if (!IsNumberNegative(num) && IsNumberNegative(other)) {
+		std::string tmpnum = other.num;
+		tmpnum.erase(0, 1);
+		return Number(Number(num) - tmpnum);
+	}
+	if (IsNumberNegative(num) && IsNumberNegative(other)) {
+		std::string tmpnum1 = num;
+		std::string tmpnum2 = other.num;
+		tmpnum1.erase(0, 1);
+		tmpnum2.erase(0, 1);
+		return Number('-' + (Number(tmpnum1) + Number(tmpnum2)).num);
+	}
+	if (num.size() > 1 && num[0] == '0') {
+		std::string tmpnum1 = num;
+		tmpnum1.erase(0, 1);
+		return Number(tmpnum1) / other;
+	}
+	if (other.num.size() > 1 && other.num[0] == '0') {
+		std::string tmpnum2 = other.num;
+		tmpnum2.erase(0, 1);
+		return *this / Number(tmpnum2);
+	}
 	std::string tmpstr = other.num;
 	Number outputCountstr = { "" };
-	Number PartialResult = { "0" };
-	//	Number tmpCountstr02 = { "" };
 	Number tmpCountstr01 = { "" };//被除数用于计算的部分
 	for (int i = 0; i < num.size(); i++) {
-		tmpCountstr01.num = tmpCountstr01.num +num[i];
+		tmpCountstr01.num = tmpCountstr01.num + num[i];
 		if (tmpCountstr01 - Number(tmpstr) < Number("0")) {
-			outputCountstr.num = outputCountstr.num + '0';
+			if (!outputCountstr.num.empty())
+				outputCountstr.num = outputCountstr.num + '0';
 			continue;
 		}
 		else {
@@ -282,11 +322,11 @@ Number Number::operator/(const Number& other) const {
 			tmpCountstr01 = tmpCountstr01 + Number(tmpstr);
 		}
 	}
-	while (outputCountstr.num.size() >= 2&& outputCountstr.num[0] == '0') {
+	while (outputCountstr.num.size() >= 2 && outputCountstr.num[0] == '0') {
 		outputCountstr.num.erase(0, 1);
 	}
-	/*if (outputCountstr.num.empty())
-		outputCountstr.num = "0";*/
+	if (outputCountstr.num.empty())
+		outputCountstr.num = "0";
 	return outputCountstr;
 }
 
