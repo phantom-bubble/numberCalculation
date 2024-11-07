c1309338230bda408f5d56108af1a07cb3d3c432
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-11-07 16:13:40 +0800

    Update Number-add-001-zzz.cpp

--- tests/Number-add-001-zzz.cpp
+++ tests/Number-add-001-zzz.cpp
@@ -5,5 +5,10 @@
 int main() {
   ASSERT((Number{"1"} + Number{"1"}).num == "2");
   ASSERT((Number{"999999999"} + Number{"1"}).num == "1000000000");
+  for(int i = -1000000; i < 1000000; ++i) {
+    for(int j = -1000000; j < 1000000; ++j) {
+      ASSERT((Number{std::to_string(i)} + Number{std::to_string(j)}).num == std::to_string(i + j));
+    }
+  }
   return 0;
 }
