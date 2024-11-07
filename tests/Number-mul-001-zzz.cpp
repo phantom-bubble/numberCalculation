29b7416f6f56c03ccce7dcf16959d01aeb7a3046
zzzzzzzZz-chow <1416271215@qq.com>
2024-11-07 22:40:49 +0800

    assert

--- tests/Number-mul-001-zzz.cpp
+++ tests/Number-mul-001-zzz.cpp
@@ -2,8 +2,8 @@
 #include "assert.h"
 
 int main() {
-    ASSERT((Number{ "1" } * Number{ "1" }).num == "1");
-    ASSERT((Number{ "999999999" } * Number{ "0" }).num == "0");
+    ASSERT((Number{ "1" } * Number{ "1" }).num, "1");
+    ASSERT((Number{ "999999999" } * Number{ "0" }).num, "0");
     for (int i = -10000; i < 10000; ++i) {
         for (int j = -10000; j < 10000; ++j) {
             ASSERT((Number{ std::to_string(i) } * Number{ std::to_string(j) }).num, std::to_string(i * j));
--- tests/Number-sub-001-zzz.cpp
+++ tests/Number-sub-001-zzz.cpp
@@ -2,8 +2,8 @@
 #include "assert.h"
 
 int main() {
-  ASSERT((Number{"1"} - Number{"1"}).num == "0");
-  ASSERT((Number{"1000000000"} - Number{"1"}).num == "999999999");
+  ASSERT((Number{"1"} - Number{"1"}).num, "0");
+  ASSERT((Number{"1000000000"} - Number{"1"}).num, "999999999");
   for(int i = -10000; i < 10000; ++i) {
     for(int j = -10000; j < 10000; ++j) {
       ASSERT((Number{std::to_string(i)} - Number{std::to_string(j)}).num, std::to_string(i - j));
