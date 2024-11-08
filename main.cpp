3d6d636e43d76ce7748d45b7bf8b25ee50cb6835
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-11-08 15:17:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@
 int main() {
 
     std::cout  << "Ehlo, word!" << std::endl;
-    Rational r1("91");
-    Rational r2("7");
-    Rational r3 = r1 / r2;
-    std::cout << r3.numerator.num <<"/"<<r3.denominator.num << std::endl;
+    Rational r1("2");
+    Rational r3 = (r1 - {"1"}) / {"1"} - (r1 - {"1"}) * (r1 - {"1"}) / {"2"} + (r1 - {"1"}) * (r1 - {"1"}) * (r1 - {"1"}) / {"6"};
+    std::cout << r3.to_ratio_string() << std::endl;
     return 0;
 }
