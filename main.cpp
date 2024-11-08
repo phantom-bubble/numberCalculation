0069b57cf3427a987396b342122680cd8f4e3a3c
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-11-08 15:49:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int main() {
     std::cout  << "Ehlo, word!" << std::endl;
     Rational r1("1.1");
     Rational r2 = r1 - Rational{"1"};
-    Rational r3 = r2 / Rational{"1"} - pow(r2, 2) / Rational{"2"} + pow(r2, 3) / Rational{"6"} - pow(r2, 4) / Rational{"24"} + pow(r2, 5) / Rational{"120"}
-        - pow(r2, 6) / Rational{"720"} + pow(r2, 7) / Rational{"5040"} - pow(r2, 8) / Rational{"40320"} +  pow(r2, 9) / Rational{"362880"};
+    Rational r3 = r2 / Rational{"1"} - pow(r2, 2) / Rational{"2"} + pow(r2, 3) / Rational{"6"} - pow(r2, 4) / Rational{"24"} + pow(r2, 5) / Rational{"120"};
+        // - pow(r2, 6) / Rational{"720"} + pow(r2, 7) / Rational{"5040"} - pow(r2, 8) / Rational{"40320"} +  pow(r2, 9) / Rational{"362880"};
     std::cout << r3.to_ratio_string() << std::endl;
     return 0;
 }
