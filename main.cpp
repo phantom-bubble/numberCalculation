278788f28bbd3c443db4cbbf906b2fce52718d41
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-11-08 15:22:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main() {
 
     std::cout  << "Ehlo, word!" << std::endl;
     Rational r1("2");
-    Rational r3 = (r1 - Rational{"1"}) / Rational{"1"} - (r1 - Rational{"1"}) * (r1 - Rational{"1"}) / Rational{"2"} + (r1 - Rational{"1"}) * (r1 - Rational{"1"}) * (r1 - Rational{"1"}) / Rational{"6"};
+    Rational r2 = r1 - Rational{"1"};
+    Rational r3 = r2 / Rational{"1"} - r2 * r2 / Rational{"2"} + r2 * r2 * r2 / Rational{"6"} - r2 * r2 * r2 * r2 / Rational{"24"} + r2 * r2 * r2 * r2 * r2 / Rational{"120"};
     std::cout << r3.to_ratio_string() << std::endl;
     return 0;
 }
