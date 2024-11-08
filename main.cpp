a4d250a04a2774c5411d60d41ace27742470c218
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-11-08 15:43:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ Rational pow(const Rational & r, int n) {
 int main() {
 
     std::cout  << "Ehlo, word!" << std::endl;
-    Rational r1("2");
+    Rational r1("1");
     Rational r2 = r1 - Rational{"1"};
     Rational r3 = r2 / Rational{"1"} - pow(r2, 2) / Rational{"2"} + pow(r2, 3) / Rational{"6"} - pow(r2, 4) / Rational{"24"} + pow(r2, 5) / Rational{"120"}
         - pow(r2, 6) / Rational{"720"} + pow(r2, 7) / Rational{"5040"} - pow(r2, 8) / Rational{"40320"} +  pow(r2, 9) / Rational{"362880"};
